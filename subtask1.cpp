d8370100c1a0796c0db1e8d00b65934edb024c40
Gunjaniitd <gunjan3723@gmail.com>
2021-02-22 00:03:00 +0530

    added mouse click to the code

--- subtask1.cpp
+++ subtask1.cpp
@@ -3,16 +3,31 @@
 using namespace cv;
 using namespace std;
 
+void on_mouse( int event, int x, int y, int flag, void *ptr )
+{
+    if (event == EVENT_LBUTTONDOWN )
+    { 
+        vector<Point2f>*pts_src = (vector<Point2f>*)ptr;
+        pts_src->push_back(Point2f(x,y));
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 	Mat traffic;
 	traffic = imread("traffic.jpg", IMREAD_GRAYSCALE);
-    // Four corners of the book in source image
     vector<Point2f> pts_src;
-    pts_src.push_back(Point2f(900,250));
-    pts_src.push_back(Point2f(150,980));
-    pts_src.push_back(Point2f(1570,980));
-    pts_src.push_back(Point2f(1300,250));
+    // Four corners of the book in source image
+    namedWindow("My Window", WINDOW_NORMAL);
+    setMouseCallback("My Window", on_mouse, &pts_src);
+    imshow("My Window", traffic);
+    waitKey(0);
+    destroyAllWindows();
+    // vector<Point2f> pts_src;
+    // pts_src.push_back(Point2f(900,250));
+    // pts_src.push_back(Point2f(150,980));
+    // pts_src.push_back(Point2f(1570,980));
+    // pts_src.push_back(Point2f(1300,250));
 
     vector<Point2f> pts_dest;
     pts_dest.push_back(Point2f(472,52));
@@ -24,7 +39,7 @@ int main(int argc, char const *argv[])
     Mat t_out, t_out_cropped;
     warpPerspective(traffic, t_out, h, traffic.size());
 
-    Rect crop(447, 10, 400, 700);
+    Rect crop(447, 10, 400, 800);
     t_out_cropped = t_out(crop);
 
     namedWindow("Warped", WINDOW_NORMAL);
